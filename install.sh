81db216d533bca031128f0de5d48869b718ca18e
MRpi5hi <88733602+MRpi5hi@users.noreply.github.com>
2021-08-10 21:13:24 +0430

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+# print banner
+ banner_path="$PWD/lib/banner"
+cat $banner_path
+echo "closing moc..."
+killall moc
+echo "seting music directory..."
+rm -f ~/.moc/last_directory
+echo $1 >> ~/.moc/last_directory
+echo "copying theme to /usr/share/moc/themes ..."
+theme_path="$PWD/lib/pi5hi_theme"
+sudo cp $theme_path /usr/share/moc/themes
+echo "set the config..."
+rm -f ~/.moc/config
+echo "Theme = /usr/share/moc/themes/pi5hi_theme" >> ~/.moc/config
+echo "XTermTheme = /usr/share/moc/themes/pi5hi_theme" >> ~/.moc/config
+
+
+
